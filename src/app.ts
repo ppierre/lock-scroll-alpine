82ac850ad58a6674991065ab176dfd4612c1d40c
pierre pracht <pierre.pracht@gmail.com>
2022-06-21 10:04:59 +0200

    code ts en fichier externe avec tous les import

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import Alpine from "alpinejs";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
+import "@fontsource/advent-pro";
+import "./index.css";
+// window.Alpine = Alpine;
+Object.assign(window, {
+  Alpine,
+  disablePageScroll,
+  enablePageScroll,
+});
+Alpine.start();
